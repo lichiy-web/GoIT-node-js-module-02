06b03461b7270f80ca495df3e4b3ee1603f92cad
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-03-24 16:19:05 +0200

    create students and students/:studenyId routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { pinoHttp } from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllStudents, getStudentById } from './services/students.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 export const startServer = () => {
@@ -20,6 +21,32 @@ export const startServer = () => {
 
   app.get('/', (req, res) => {
     res.json({ message: 'Hello world!' });
+  });
+
+  app.get('/students', async (req, res) => {
+    const students = await getAllStudents();
+
+    res.status(200).json({
+      data: students,
+    });
+  });
+
+  app.get('/students/:studentId', async (req, res, next) => {
+    const { studentId } = req.params;
+    const student = await getStudentById(studentId);
+
+    // Відповідь, якщо контакт не знайдено
+    if (!student) {
+      res.status(404).json({
+        message: 'Student not found',
+      });
+      return;
+    }
+
+    // Відповідь, якщо контакт знайдено
+    res.status(200).json({
+      data: student,
+    });
   });
 
   app.use('*', (req, res, next) => {
