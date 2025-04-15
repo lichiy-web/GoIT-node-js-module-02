29a4a46a5c3d6a4273afc99a80f61e15efc287fe
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-04-15 20:58:14 +0300

    add multer

--- src/routers/students.js
+++ src/routers/students.js
@@ -15,27 +15,46 @@ import {
   updateStudentSchema,
 } from '../validation/students.js';
 import { authenticate } from '../middleware/authenticate.js';
+import { upload } from '../middleware/multer.js';
+import { checkRoles } from '../middleware/checkRoles.js';
+import { ROLES } from '../constants/index.js';
 
 const router = new Router();
 router.use(authenticate);
 
-router.get('/', ctrlWrapper(getStudentsController));
-router.get('/:studentId', isValidId, ctrlWrapper(getStudentByIdController));
+router.get('/', checkRoles(ROLES.TEACHER), ctrlWrapper(getStudentsController));
+router.get(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
+  isValidId,
+  ctrlWrapper(getStudentByIdController),
+);
 router.post(
   '/',
+  checkRoles(ROLES.TEACHER),
+  upload.single('photo'),
   validateBody(createStudentSchema),
   ctrlWrapper(createStudentController),
 );
-router.delete('/:studentId', isValidId, ctrlWrapper(deleteStudentController));
+router.delete(
+  '/:studentId',
+  checkRoles(ROLES.TEACHER),
+  isValidId,
+  ctrlWrapper(deleteStudentController),
+);
 router.put(
   '/:studentId',
+  checkRoles(ROLES.TEACHER),
   isValidId,
+  upload.single('photo'),
   validateBody(createStudentSchema),
   ctrlWrapper(upsertStudentController),
 );
 router.patch(
   '/:studentId',
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
   isValidId,
+  upload.single('photo'),
   validateBody(updateStudentSchema),
   ctrlWrapper(patchStudentController),
 );
